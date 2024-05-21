31b84dfade2cb260195b34f4d27f69d7afb4e812
17300 <2532349769@qq.com>
2024-05-21 11:46:48 +0800

    修改

--- yolov8_gold_train.py
+++ yolov8_gold_train.py
@@ -6,5 +6,5 @@ model = YOLO('yolov8-gold.yaml')  # build a new model from YAML
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='coco-8pose.yaml',epochs=20, imgsz=640)
+results = model.train(data='coco8-pose.yaml',epochs=20, imgsz=640)
 # results = model.predict('/root/ultralytics/ultralytics/assets/bus.jpg',save=True)
